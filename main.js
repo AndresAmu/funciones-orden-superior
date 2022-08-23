84c8bb1a53db572e6de12604f87cf943a6d6dc04
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-08-23 17:39:24 -0300

    .findIndex

--- main.js
+++ main.js
@@ -84,3 +84,26 @@ console.log(puntos2)
 let puntos3 = [1, 12, 53, 4, 13, 19, 61, 12, 6]
 puntos3.sort((a, b) => a - b)
 console.log(puntos3)
+
+//.find devuelve el valor del primer elemento
+
+const array1 = [5, 3, 1, 64, 125, 6, 43]
+
+//Forma larga
+const found1 = array1.find(function(e){
+    return e> 3
+})
+
+console.log(found1)
+
+//Forma corta =>
+const found = array1.find(e => e > 20)
+console.log(found)
+
+//.findIndex devuelve el indice del primer elemento segun la condicion
+
+const arrays = [12, 41, 5, 23, 2]
+
+const finds = arrays.findIndex(e => e < 3)
+console.log(finds)
+
