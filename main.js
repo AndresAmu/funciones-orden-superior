ec154e0381bb1ebf3c21b16106b263e8f2d6f649
Andres Amuchastegui <amuchastegui1994@gmail.com>
2022-08-23 16:03:05 -0300

    funciones

---
+++ main.js
@@ -0,0 +1,8 @@
+//Funciones de orden superior
+
+//.map
+var numbers= [1,5,10,15];
+var doubles = numbers.map(function(x){ // x sustituye cada elemento del array numers
+    return x *2;
+})
+console.log(doubles)
